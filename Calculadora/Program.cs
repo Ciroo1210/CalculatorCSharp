fda243562c2bd84a40118ab088285c62be09d88b
Ciro <cdante2022@gmail.com>
2024-05-24 21:55:44 -0300

    raiz cuadrada y potencia

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,13 +1,13 @@
 ﻿try
 {
-    int num1;
-    int num2;
-    int result;
+    double num1;
+    double num2;
+    double result;
     char again = 'y';
 
     while (again == 'y')
     {
-        Console.WriteLine("Select an operation: \n1. Add \n2. Subtract \n3. Multiply \n4. Divide");
+        Console.WriteLine("Select an operation: \n1. Add \n2. Subtract \n3. Multiply \n4. Divide \n5. Power \n6. Square Root");
         string election = Console.ReadLine();
         switch (election)
         {
@@ -23,6 +23,12 @@
             case "4":
                 Divide();
                 break;
+            case "5":
+                Power();
+                break;
+            case "6":
+                squareRoot();
+                break;
             default:
                 Console.WriteLine("Invalid input. Please select a valid operation.");
                 break;
@@ -31,8 +37,8 @@
         void Add()
         {
             Console.WriteLine("Enter the values to add:");
-            num1 = Int32.Parse(Console.ReadLine());
-            num2 = Int32.Parse(Console.ReadLine());
+            num1 = Double.Parse(Console.ReadLine());
+            num2 = Double.Parse(Console.ReadLine());
             result = num1 + num2;
             Console.WriteLine("Result: " + result);
             Finish();
@@ -40,8 +46,8 @@
         void Subtract()
         {
             Console.WriteLine("Enter the values to subtract:");
-            num1 = Int32.Parse(Console.ReadLine());
-            num2 = Int32.Parse(Console.ReadLine());
+            num1 = Double.Parse(Console.ReadLine());
+            num2 = Double.Parse(Console.ReadLine());
             result = num1 - num2;
             Console.WriteLine("Result: " + result);
             Finish();
@@ -49,8 +55,8 @@
         void Multiply()
         {
             Console.WriteLine("Enter the values to multiply:");
-            num1 = Int32.Parse(Console.ReadLine());
-            num2 = Int32.Parse(Console.ReadLine());
+            num1 = Double.Parse(Console.ReadLine());
+            num2 = Double.Parse(Console.ReadLine());
             result = num1 * num2;
             Console.WriteLine("Result: " + result);
             Finish();
@@ -58,8 +64,8 @@
         void Divide()
         {
             Console.WriteLine("Enter the values to divide:");
-            num1 = Int32.Parse(Console.ReadLine());
-            num2 = Int32.Parse(Console.ReadLine());
+            num1 = Double.Parse(Console.ReadLine());
+            num2 = Double.Parse(Console.ReadLine());
             if (num2 == 0)
             {
                 Console.WriteLine("Error: Division by zero is not allowed.");
@@ -71,17 +77,36 @@
             }
             Finish();
         }
+        void Power()
+        {
+            Console.WriteLine("Enter the base value:");
+            num1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the exponent value:");
+            num2 = Double.Parse(Console.ReadLine());
+            result = Math.Pow(num1, num2);
+            Console.WriteLine("Result: " + result);
+            Finish();
+        }
+        void squareRoot()
+        {
+            num1 = Double.Parse(Console.ReadLine());
+            double result = Math.Sqrt(num1);
+            Console.WriteLine(result);
+            
+            
+        }
         void Finish()
         {
             Console.WriteLine("Do you want to continue? (y/n)");
-            string ContinueVar = Console.ReadLine().ToLower();
-            if (ContinueVar == "n")
+            string continueVar = Console.ReadLine().ToLower();
+            if (continueVar == "n")
             {
                 again = 'n';
             }
-            else if (ContinueVar == "y")
+            else if (continueVar == "y")
             {
                 again = 'y';
+                Console.Clear();
             }
             else
             {
